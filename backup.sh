43c3a88b018b0aac6a23c8156882514dc489fd39
Martin Higham <martin@ocastastudios.co.uk>
2016-06-16 16:16:32 +0100

    Add $SQLUSER & $SQLPASSWORD

--- backup.sh
+++ backup.sh
@@ -31,7 +31,7 @@ fi
 export AWS_ACCESS_KEY_ID=$S3_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$S3_SECRET_ACCESS_KEY
 
-MYSQL_HOST_OPTS=" --user root --encrypt=AES256 --encrypt-key=$ENCRYPT_KEY --stream=xbstream --compress ."
+MYSQL_HOST_OPTS=" --user $SQLUSER --password $SQLPASSWORD --encrypt=AES256 --encrypt-key=$ENCRYPT_KEY --stream=xbstream --compress ."
 
 echo "Creating backup of ${MYSQLDUMP_DATABASE} database(s) from ${MYSQL_HOST}..."
 
